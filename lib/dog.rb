06495ceaf3463c5a00f19b47f3051de8b9f7740c
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-19 01:34:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,20 +5,4 @@ class Dog
     @breed = "Mutt"
   end
   
-  def name
-    @name
-  end
-  
-  def name=(new_name)
-    @name = new_name
-  end
-  
-  def breed
-    @breed
-  end
-  
-  def breed=(new_breed)
-    @breed = new_breed
-  end
-  
 end
